a7e446477731ffbff03976ee80ae4a1a45c2b43f
Quphoria <s.simpson41.ss@googlemail.com>
2026-04-28 00:28:18 +0100

    Edit fonts.cmake

--- firmware/custom_font/fonts.cmake
+++ firmware/custom_font/fonts.cmake
@@ -10,6 +10,8 @@ set(Pix32_Inv_Font_H_PATH ${GEN_DIR}/Pix32_Inv_Font.h)
 set(Chan_Icons_H_PATH ${GEN_DIR}/Chan_Icons.h)
 set(On_Icon_H_PATH ${GEN_DIR}/On_Icon.h)
 
+set(GEN_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/font_to_header.py)
+
 add_library(fonts INTERFACE)
 target_include_directories(fonts INTERFACE ${GEN_DIR})
 
@@ -33,8 +35,8 @@ add_custom_target(FONT_VENV DEPENDS font_venv/ready)
 
 add_custom_command(
     OUTPUT ${Pix32_Font_H_PATH}
-    DEPENDS ${Pix32_Font_PATH} font_venv/ready FONT_VENV # target level dependency to avoid race condition
-    COMMAND ./font_venv/${VENV_BIN_DIR}/python "${CMAKE_CURRENT_LIST_DIR}/font_to_header.py"
+    DEPENDS ${Pix32_Font_PATH} ${GEN_SCRIPT} font_venv/ready FONT_VENV # target level dependency to avoid race condition
+    COMMAND ./font_venv/${VENV_BIN_DIR}/python "${GEN_SCRIPT}"
             -W 6 -H 12
             "${Pix32_Font_PATH}"
             -o "${Pix32_Font_H_PATH}"
@@ -45,8 +47,8 @@ add_dependencies(fonts Pix32_Font)
 
 add_custom_command(
     OUTPUT ${Pix32_Inv_Font_H_PATH}
-    DEPENDS ${Pix32_Font_PATH} font_venv/ready FONT_VENV # target level dependency to avoid race condition
-    COMMAND ./font_venv/${VENV_BIN_DIR}/python "${CMAKE_CURRENT_LIST_DIR}/font_to_header.py"
+    DEPENDS ${Pix32_Font_PATH} ${GEN_SCRIPT} font_venv/ready FONT_VENV # target level dependency to avoid race condition
+    COMMAND ./font_venv/${VENV_BIN_DIR}/python "${GEN_SCRIPT}"
             -W 6 -H 12 -I
             "${Pix32_Font_PATH}"
             -o "${Pix32_Inv_Font_H_PATH}"
@@ -57,8 +59,8 @@ add_dependencies(fonts Pix32_Inv_Font)
 
 add_custom_command(
     OUTPUT ${On_Icon_H_PATH}
-    DEPENDS ${On_Icon_PATH} font_venv/ready FONT_VENV # target level dependency to avoid race condition
-    COMMAND ./font_venv/${VENV_BIN_DIR}/python "${CMAKE_CURRENT_LIST_DIR}/font_to_header.py"
+    DEPENDS ${On_Icon_PATH} ${GEN_SCRIPT} font_venv/ready FONT_VENV # target level dependency to avoid race condition
+    COMMAND ./font_venv/${VENV_BIN_DIR}/python "${GEN_SCRIPT}"
             -W 13 -H 11 -s 65 -c 1
             "${On_Icon_PATH}"
             -o "${On_Icon_H_PATH}"
@@ -69,8 +71,8 @@ add_dependencies(fonts On_Icon)
 
 add_custom_command(
     OUTPUT ${Chan_Icons_H_PATH}
-    DEPENDS ${Chan_Icons_PATH} font_venv/ready FONT_VENV # target level dependency to avoid race condition
-    COMMAND ./font_venv/${VENV_BIN_DIR}/python "${CMAKE_CURRENT_LIST_DIR}/font_to_header.py"
+    DEPENDS ${Chan_Icons_PATH} ${GEN_SCRIPT} font_venv/ready FONT_VENV # target level dependency to avoid race condition
+    COMMAND ./font_venv/${VENV_BIN_DIR}/python "${GEN_SCRIPT}"
             -W 32 -H 16 -s 65 -c 5
             "${Chan_Icons_PATH}"
             -o "${Chan_Icons_H_PATH}"
