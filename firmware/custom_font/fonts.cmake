7a38064816535ac5d57df22dcfb210da5a762d3d
Quphoria <s.simpson41.ss@googlemail.com>
2026-04-27 02:46:29 +0100

    Fix font_venv race condition

--- firmware/custom_font/fonts.cmake
+++ firmware/custom_font/fonts.cmake
@@ -28,11 +28,12 @@ add_custom_command(
     COMMAND ./font_venv/${VENV_BIN_DIR}/pip install -r "${CMAKE_CURRENT_LIST_DIR}/requirements.txt"
     COMMAND echo "ready" > ./font_venv/ready # signify the install was a success
 )
+add_custom_target(FONT_VENV DEPENDS font_venv/ready)
 
 
 add_custom_command(
     OUTPUT ${Pix32_Font_H_PATH}
-    DEPENDS ${Pix32_Font_PATH} font_venv/ready
+    DEPENDS ${Pix32_Font_PATH} font_venv/ready FONT_VENV # target level dependency to avoid race condition
     COMMAND ./font_venv/${VENV_BIN_DIR}/python "${CMAKE_CURRENT_LIST_DIR}/font_to_header.py"
             -W 6 -H 12
             "${Pix32_Font_PATH}"
@@ -44,7 +45,7 @@ add_dependencies(fonts Pix32_Font)
 
 add_custom_command(
     OUTPUT ${Pix32_Inv_Font_H_PATH}
-    DEPENDS ${Pix32_Font_PATH} font_venv/ready
+    DEPENDS ${Pix32_Font_PATH} font_venv/ready FONT_VENV # target level dependency to avoid race condition
     COMMAND ./font_venv/${VENV_BIN_DIR}/python "${CMAKE_CURRENT_LIST_DIR}/font_to_header.py"
             -W 6 -H 12 -I
             "${Pix32_Font_PATH}"
@@ -56,7 +57,7 @@ add_dependencies(fonts Pix32_Inv_Font)
 
 add_custom_command(
     OUTPUT ${On_Icon_H_PATH}
-    DEPENDS ${On_Icon_PATH} font_venv/ready
+    DEPENDS ${On_Icon_PATH} font_venv/ready FONT_VENV # target level dependency to avoid race condition
     COMMAND ./font_venv/${VENV_BIN_DIR}/python "${CMAKE_CURRENT_LIST_DIR}/font_to_header.py"
             -W 13 -H 11 -s 65 -c 1
             "${On_Icon_PATH}"
@@ -68,7 +69,7 @@ add_dependencies(fonts On_Icon)
 
 add_custom_command(
     OUTPUT ${Chan_Icons_H_PATH}
-    DEPENDS ${Chan_Icons_PATH} font_venv/ready
+    DEPENDS ${Chan_Icons_PATH} font_venv/ready FONT_VENV # target level dependency to avoid race condition
     COMMAND ./font_venv/${VENV_BIN_DIR}/python "${CMAKE_CURRENT_LIST_DIR}/font_to_header.py"
             -W 32 -H 16 -s 65 -c 5
             "${Chan_Icons_PATH}"
